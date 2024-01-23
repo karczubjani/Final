d285c8a6d68000186c101357272197974f5afa80
karczubjani <k.jano1999@gmail.com>
2024-01-23 15:13:52 +0100

    home

---
+++ Final/resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title>Profil</title>
+</head>
+<body class="d-flex justify-content-center align-items-center m-5" style="background-color: rgba(0,0,255,.1)">
+    <div>ÜDV</div>
+</body>
+</html>
