4851db5b27d4b93778935375d962ef7ddbf8f4bb
karczubjani <k.jano1999@gmail.com>
2024-01-23 15:50:47 +0100

    update

--- Final/routes/web.php
+++ Final/routes/web.php
@@ -22,5 +22,4 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-
 Route::get('get-countries', [CountryController::class, 'getCountries']);
